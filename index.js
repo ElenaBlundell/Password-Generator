5e5eb9418372502060f2fe52942a372779fb8bb1
Elena Blundell <blundell.elena@gmail.com>
2023-10-15 19:44:26 -0500

    added eventListener to the generate button

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let password1 = document.getElementById("password-1")
-let password2 = document.getElementById("password-2")
+const generateBtn = document.getElementById("generate-btn")
+const password1 = document.getElementById("password-1")
+const password2 = document.getElementById("password-2")
+
 
 function randomPassword() {
     let password = ""
@@ -17,3 +19,5 @@ function generatePasswords() {
     password1.textContent = randomPassword()
     password2.textContent = randomPassword()
 }
+
+generateBtn.addEventListener("click", generatePasswords)
